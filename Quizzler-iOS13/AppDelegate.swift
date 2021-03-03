a1c9b164bae8d501c722105aa545a5ea996f2dd4
Dylan Park <dylanpark.design@gmail.com>
2021-03-03 14:52:48 +1000

    Update AppDelegate.swift

--- Quizzler-iOS13/AppDelegate.swift
+++ Quizzler-iOS13/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Quizzler-iOS13
 //
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Dylan Park on 2/2/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
 //
 
 import UIKit
